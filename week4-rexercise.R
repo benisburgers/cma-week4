27856305c7646e1a936c3c9751c5ee14702d60dd
benisburgers <benibargera@gmail.com>
2022-05-13 11:35:18 +0200

    Complete task 2

--- week4-rexercise.R
+++ week4-rexercise.R
@@ -1,7 +1,3 @@
-# Load libraries & Import Data
-
-library(tidyverse)
-
 # Task 1: Write your own functions
 
 ## Create a function for our Euclidean distance calculation.
@@ -13,3 +9,35 @@ calc_dist <- function(point1, point2) {
 
 test_calc_dist <- calc_dist(c(10, 15), c(10, 10))
 test_calc_dist
+
+# Task 2: Prepare Analysis
+
+## Load libraries
+
+library(readr)        
+library(dplyr)        
+library(ggplot2)      
+library(lubridate)
+
+## Import Data
+
+wildschwein <- read_delim('wildschwein_BE_2056.txt')
+wildschwein
+
+## Filter Data
+
+### Check how many individuals & the date range
+
+unique(wildschwein$TierName)
+min(wildschwein$DatetimeUTC)
+max(wildschwein$DatetimeUTC)
+
+### Filter by name & date
+
+wildschwein_filtered <- wildschwein %>%
+  filter(TierName == "Rosa" | TierName == "Sabi") %>%
+  filter(DatetimeUTC >= "2015-04-01" & DatetimeUTC <= "2015-04-15")
+
+unique(wildschwein_filtered$TierName)
+min(wildschwein_filtered$DatetimeUTC)
+max(wildschwein_filtered$DatetimeUTC)
